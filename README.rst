e707824328ee276c8be4640bcc63aedf1abb5b8b
Guillaume <guillaume.schworer@gmail.com>
2017-01-18 11:08:16 +0100

    license

---
+++ README.rst
@@ -0,0 +1,8 @@
+.. ctrl
+
+:Name: ctrl
+:Website: https://git.obspm.fr/git/projets/Picsat/segsol
+:Author: Guillaume Schworer
+:Version: 0.1
+
+Ground-segment software of the PicSat project.
--- ctrl/param/param_all.py
+++ ctrl/param/param_all.py
@@ -1,12 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+###############################################################################
+#  
+#  CTRL - Ground-Segment software for Cube-Sats
+#  Copyright (C) 2016-2017  Guillaume Schworer
+#  
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#  
+#  For any information, bug report, idea, donation, hug, beer, please contact
+#    guillaume.schworer@gmail.com
+#
+###############################################################################
+
+
 NOERRORATIMPORT = True
 
 # the path to the file containing the packet id counter
 PACKETIDFILE = ["param", "tc_packet_id"]
 
-# the path to the parameter file containing the DB connection settinsg
+# the path to the parameter file containing the DB connection settings
 DBFILE = ["param", "db_server"]
 
 # who is the emitter
