c8cde8c87ade0c1e2cec6a2d83fda913c20399c8
Guillaume <guillaume.schworer@gmail.com>
2017-04-05 17:24:16 +0200

    sent_TC

--- ctrl/db/orm.py
+++ ctrl/db/orm.py
@@ -120,6 +120,7 @@ def update_sent_TC_time(pkid, t):
     idx = DB.query(TC.id).filter_by(packet_id=pkid)\
             .order_by(TC.id.desc()).limit(1).with_for_update()
     update(TC).values({'time_sent': t}).where(TC.id == idx.as_scalar())
+    print idx.first()[0]
     return idx.first()[0]
 
 
