e71f87f359ca5efaf0bb67c7718f0ebb9684ecb0
Guillaume <guillaume.schworer@gmail.com>
2017-04-07 22:48:24 +0200

    disp TM/TC show

--- ctrl/xdisp/watchdog.py
+++ ctrl/xdisp/watchdog.py
@@ -60,8 +60,8 @@ L0ICO = u'\u2218'
 L1ICO = u'\u25CE'
 HORLINE = u'\u2500'
 
-TCFMT = u'{timestamp} {pld} {lvl} #{pid:<15} {pkid:>5} {cmd_name:<27}'
-TMFMT = u'{timestamp} {pld} {lvl} #{pid:<15} {pkid:>5} cat: {cat:>2} len: {sz:>3}'
+TCFMT = u'{timestamp} {pld} {lvl} {pid:<15} #{pkid:>5} {cmd_name:<27}'
+TMFMT = u'{timestamp} {pld} {lvl} {pid:<15} #{pkid:>5} cat: {cat:>2} len: {sz:>3}'
 MAXSTORETC = 100
 MAXDISPLAYTC = 8
 MAXSTORETM = 100
