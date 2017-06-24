83bd35f6be862d35c2ea4dd8dcd7f3630583dc65
segsol <picsat.office@obspm.fr>
2017-06-24 23:05:03 +0200

    configure rtc

--- ctrl/cmd/cmd_patch.py
+++ ctrl/cmd/cmd_patch.py
@@ -84,3 +84,52 @@ class genericCrcPatch(CommandPatch):
         packet = packet[:param_ccsds.HEADER_P_KEYS.size+\
                         param_ccsds.HEADER_S_KEYS_TELECOMMAND.size] + data
         return packet, hd, hdx, inputs
+
+
+# real time clock at bootloader level
+class configureRTC(CommandPatch):
+    _crcParamName = 'crc'
+
+    def generate_data(self, *args, **kwargs):
+        """
+        This command has been patched.
+        Convert a datetime object to an acceptable format, and sent it 
+        to the payload for updating the Real Time Clock. 
+        Very useful in combination with datetime.datetime.utcnow().
+        @param datetime datetime: the datetime or tuple object to convert. 
+        """
+        # force 0 sur le crc
+        stamp = kwargs.get('datetime')
+        if stamp is None:
+            raise Exception("datetime is not an optional argument!")
+        if not isinstance(stamp, (datetime, list, tuple)):
+            raise TypeError
+        if isinstance(stamp, (list, tuple)):
+            stamp = datetime(*stamp)
+        newkwargs = {}
+        newkwargs['years'] = stamp.year - 1970
+        newkwargs['months'] = stamp.month
+        newkwargs['days'] = stamp.day
+        newkwargs['hours'] = stamp.hour
+        newkwargs['minutes'] = stamp.minute                
+        newkwargs['seconds'] = stamp.second
+        kwargs[self._crcParamName] = 0
+        return super(configureRTC, self).generate_data(*args, **newkwargs)
+    
+    def _generate_packet(self, *args, **kwargs):
+        # appel méthode mère
+        packet, hd, hdx, inputs = super(genericCrcPatch, self)\
+                                  ._generate_packet(*args, **kwargs)
+        # on calc le CRC sur le header sec et les data
+        bytesForCrc = packet[param_ccsds.HEADER_P_KEYS.size:-4]
+        crc = core.payload_crc32(bytesForCrc)
+        # on remplace le crc dans les inputs, parce que voilà
+        inputs[self._crcParamName] = crc
+        # on ré-encode les param de la command avec la méthode mère
+        data, inputs = super(genericCrcPatch, self).generate_data(*args,
+                                                            **inputs)
+        # on remplace les data dans le packet encodé
+        packet = packet[:param_ccsds.HEADER_P_KEYS.size+\
+                        param_ccsds.HEADER_S_KEYS_TELECOMMAND.size] + data
+        return packet, hd, hdx, inputs    
+
